fcbda14999690b3047543998282ae8c3f4c00bbf
xtechilad <ps940016@gmail.com>
2023-02-07 12:41:48 +0530

    Add .env

--- src/lib/config/firebase.js
+++ src/lib/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage }from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDkc3GC1X7-QmDftl56ogsYgRfJeOz9_zA",
-    authDomain: "telechat-b0d0c.firebaseapp.com",
-    projectId: "telechat-b0d0c",
-    storageBucket: "telechat-b0d0c.appspot.com",
-    messagingSenderId: "748298537128",
-    appId: "1:748298537128:web:7534f5c457bd5737025a9d"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
